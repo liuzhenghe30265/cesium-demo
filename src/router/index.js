71632227ff4bed48438720beb141e058bfcd2a1c
liuzhenghe30265 <15901450207@163.com>
2022-05-29 16:15:56 +0800

    fix:update

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,8 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue')
+    name: 'Init',
+    component: () => import('../views/Init.vue')
   }
 ]
 
