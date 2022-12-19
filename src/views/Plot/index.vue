48be6b0af1e0a421c9380993483294508b70b4b3
liuzhenghe30265 <15901450207@163.com>
2022-12-19 16:18:14 +0800

    fix: 根据高程绘制高低起伏的 wall

--- src/views/Plot/index.vue
+++ src/views/Plot/index.vue
@@ -7,6 +7,7 @@
 /* eslint-disable no-undef */
 import { cartesianToLongAndLat } from '@/utils/CesiumUtils.js'
 import '@/utils/dynamicWallMaterialProperty'
+import { flattenDeep } from 'lodash'
 export default {
   data() {
     return {}
@@ -68,24 +69,12 @@ export default {
     }, Cesium.ScreenSpaceEventType.LEFT_CLICK)
 
     const data = require('./mock.json')
-    data.map(item => {
+    data.map(async item => {
       if (item.objectInfo && item.objectInfo.drawingMode === 'polygon') {
         const positions = []
         const hierarchy = []
-        const maximumHeights = []
         const minimumHeights = []
-        let points = []
-        if (
-          item.objectInfo.verticesPosition &&
-          item.objectInfo.verticesPosition.length > 0
-        ) {
-          points = item.objectInfo.verticesPosition
-        } else if (
-          item.objectInfo.activeShapePoints &&
-          item.objectInfo.activeShapePoints.length > 0
-        ) {
-          points = item.objectInfo.activeShapePoints
-        }
+        const points = item.objectInfo.activeShapePoints
         points.map(point => {
           const Cartesian3 = Cesium.Cartesian3.fromDegrees(
             point.longitude,
@@ -96,20 +85,20 @@ export default {
           positions.push(point.longitude)
           positions.push(point.latitude)
           minimumHeights.push(point.altitude)
-          maximumHeights.push(point.altitude + 100)
-          viewer.entities.add({
-            name: 'plotGraphic',
-            position: Cartesian3,
-            point: {
-              color: Cesium.Color.YELLOW,
-              pixelSize: 20
-            }
-          })
+          // 顶点
+          // viewer.entities.add({
+          //   name: 'plotGraphic',
+          //   position: Cartesian3,
+          //   point: {
+          //     color: Cesium.Color.YELLOW,
+          //     pixelSize: 10
+          //   }
+          // })
         })
         positions.push(points[0].longitude)
         positions.push(points[0].latitude)
         minimumHeights.push(points[0].altitude)
-        maximumHeights.push(points[0].altitude + 100)
+        const maximumHeights = minimumHeights.map(_ => _ + 100)
         viewer.entities.add({
           name: 'plotGraphic',
           polygon: {
@@ -117,7 +106,7 @@ export default {
             // extrudedHeight: 200,
             // perPositionHeight: true,
             // heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-            material: Cesium.Color.RED.withAlpha(0.6)
+            material: Cesium.Color.RED.withAlpha(0.4)
           }
         })
         // viewer.entities.add({
@@ -125,14 +114,86 @@ export default {
         //     positions: Cesium.Cartesian3.fromDegreesArray(positions),
         //     maximumHeights: maximumHeights,
         //     minimumHeights: minimumHeights,
-        //     material: Cesium.Color.BLUE.withAlpha(0.5)
+        //     material: Cesium.Color.BLUE.withAlpha(0.3)
         //     // material: new Cesium.DynamicWallMaterialProperty({
-        //     //   color: Cesium.Color.fromCssColorString('#fff'),
+        //     //   color: Cesium.Color.fromCssColorString('#E93417FF'),
         //     //   trailImage: require('@/assets/images/colors.png'),
         //     //   duration: 1000
         //     // })
         //   }
         // })
+
+        // 用 sampleTerrainMostDetailed 在每条边上生成若干个点，绘制 wall，实现高低起伏的效果
+        const lineArray = []
+        for (let index = 0; index < points.length; index++) {
+          const element1 = points[index]
+          const element2 = points[index + 1]
+          if (element1 && element2) {
+            lineArray.push([element1, element2])
+          }
+        }
+        lineArray.push([points[points.length - 1], points[0]])
+        return Promise.all(
+          lineArray.map(point => {
+            return new Promise(resolve => {
+              const start = Cesium.Cartesian3.fromDegrees(
+                point[0].longitude,
+                point[0].latitude,
+                point[0].altitude
+              )
+              const end = Cesium.Cartesian3.fromDegrees(
+                point[1].longitude,
+                point[1].latitude,
+                point[1].altitude
+              )
+              const positions2 = [Cesium.Cartographic.fromCartesian(start)]
+              // 插值 100 个点
+              const count = 1000
+              for (let i = 1; i < count; i++) {
+                const cart = Cesium.Cartesian3.lerp(
+                  start,
+                  end,
+                  i / count,
+                  new Cesium.Cartesian3()
+                )
+                positions2.push(Cesium.Cartographic.fromCartesian(cart))
+              }
+              positions2.push(Cesium.Cartographic.fromCartesian(end))
+
+              const promise = Cesium.sampleTerrainMostDetailed(
+                Cesium.createWorldTerrain(),
+                positions2
+              )
+              Promise.resolve(promise).then(function (updatedPositions) {
+                const result = updatedPositions.map(_ => {
+                  return {
+                    longitude: Cesium.Math.toDegrees(_.longitude),
+                    latitude: Cesium.Math.toDegrees(_.latitude),
+                    altitude: _.height
+                  }
+                })
+                resolve(result)
+              })
+            })
+          })
+        ).then(res => {
+          const positions = []
+          const _res = flattenDeep(res)
+          _res.map(point => {
+            positions.push(point.longitude)
+            positions.push(point.latitude)
+          })
+          const minimumHeights = _res.map(_ => _.altitude)
+          const maximumHeights = minimumHeights.map(_ => _ + 40)
+          viewer.entities.add({
+            wall: {
+              positions: Cesium.Cartesian3.fromDegreesArray(positions),
+              maximumHeights: maximumHeights,
+              minimumHeights: minimumHeights,
+              material: Cesium.Color.BLUE.withAlpha(0.5)
+            }
+          })
+        })
       }
     })
   },
