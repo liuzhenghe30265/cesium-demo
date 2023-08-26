3412b9fbef6215db3726c53383dc2149421dd050
liuzhenghe30265 <15901450207@163.com>
2023-08-26 10:30:28 +0800

    fix: 3DTileset 中心点

--- src/views/3DTileset/index.vue
+++ src/views/3DTileset/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div style="width: 100%; height: 100%;">
-    <div id="cesium-container" style="width: 100%; height: 100%;" />
+    <div
+      id="cesium-container"
+      style="width: 100%; height: 100%;"
+    />
     <div class="btns">
       <i @click="handleClick">
         {{ cut ? '内部裁切' : '外部裁切' }}
@@ -13,6 +16,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable new-cap */
 /* eslint-disable no-unused-vars */
+import { cartesianToLongAndLat } from '@/utils/CesiumUtils/common.js'
 import { cutModelByPolygon } from '@/utils/ClippingPlane'
 export default {
   data() {
@@ -33,13 +37,104 @@ export default {
     // 添加 3DTileset
     this.tileset = new Cesium.Cesium3DTileset({
       // url: '/model/truck/good/terra_b3dms/tileset.json',
-      url: 'https://lab.earthsdk.com/model/3610c2b0d08411eab7a4adf1d6568ff7/tileset.json', // 上海（白）
-      // url: 'https://lab.earthsdk.com/model/908311a0ac2f11e99dbd8fd044883638/tileset.json', // 上海（蓝）
+      // url: 'https://lab.earthsdk.com/model/3610c2b0d08411eab7a4adf1d6568ff7/tileset.json', // 上海（白）
+      url: 'https://lab.earthsdk.com/model/908311a0ac2f11e99dbd8fd044883638/tileset.json', // 上海（蓝）
       // url: 'https://lab.earthsdk.com/model/de2a2300ac2d11e99dbd8fd044883638/tileset.json', // 大雁塔（单体）
       // url: 'https://lab.earthsdk.com/model/f15b9e90ac2d11e99dbd8fd044883638/tileset.json', // 大雁塔
       debugShowMemoryUsage: false
     })
     viewer.scene.primitives.add(this.tileset)
+    this.tileset.readyPromise
+      .then(async tileset => {
+        // 获取3D Tiles的包围盒
+        const boundingSphere = tileset.boundingSphere
+
+        // 获取包围盒的中心点
+        const center = boundingSphere.center
+
+        const result = await Cesium.sampleTerrainMostDetailed(
+          viewer.terrainProvider,
+          [center]
+        )
+        const maxHeightPosition = cartesianToLongAndLat(result[0])
+        // console.log('最高点', maxHeightPosition)
+        // viewer.entities.add(
+        //   new Cesium.Entity({
+        //     position: Cesium.Cartesian3.fromDegrees(
+        //       maxHeightPosition.longitude,
+        //       maxHeightPosition.latitude,
+        //       maxHeightPosition.altitude
+        //     ),
+        //     point: new Cesium.PointGraphics({
+        //       pixelSize: 10,
+        //       heightReference: Cesium.HeightReference.NONE,
+        //       color: new Cesium.Color.fromCssColorString('#d81e06').withAlpha(
+        //         1
+        //       ),
+        //       outlineColor: new Cesium.Color.fromCssColorString(
+        //         '#d81e06'
+        //       ).withAlpha(1),
+        //       outlineWidth: 1
+        //     }),
+        //     label: {
+        //       text: `最高点\n${maxHeightPosition.longitude}\n${maxHeightPosition.latitude}\n${maxHeightPosition.altitude}`,
+        //       style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+        //       font: '16px sans-serif',
+        //       fillColor: new Cesium.Color.fromCssColorString('#fff'),
+        //       outlineColor: new Cesium.Color.fromCssColorString('#333'),
+        //       outlineWidth: 1,
+        //       verticalOrigin: Cesium.VerticalOrigin.CENTER,
+        //       horizontalOrigin: Cesium.HorizontalOrigin.CENTER
+        //     }
+        //   })
+        // )
+
+        // 中心点坐标
+        const centerPosition = cartesianToLongAndLat(center)
+        console.log('中心点', centerPosition)
+        viewer.entities.add(
+          new Cesium.Entity({
+            position: Cesium.Cartesian3.fromDegrees(
+              centerPosition.longitude,
+              centerPosition.latitude,
+              maxHeightPosition.altitude
+            ),
+            point: new Cesium.PointGraphics({
+              pixelSize: 10,
+              heightReference: Cesium.HeightReference.NONE,
+              color: new Cesium.Color.fromCssColorString('#d81e06').withAlpha(
+                1
+              ),
+              outlineColor: new Cesium.Color.fromCssColorString(
+                '#d81e06'
+              ).withAlpha(1),
+              outlineWidth: 1
+            }),
+            label: {
+              text: `中心点\n${centerPosition.longitude}\n${centerPosition.latitude}\n${centerPosition.altitude}`,
+              style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+              font: '16px sans-serif',
+              fillColor: new Cesium.Color.fromCssColorString('#fff'),
+              outlineColor: new Cesium.Color.fromCssColorString('#333'),
+              outlineWidth: 1,
+              verticalOrigin: Cesium.VerticalOrigin.CENTER,
+              horizontalOrigin: Cesium.HorizontalOrigin.CENTER
+            }
+          })
+        )
+      })
+      .catch(error => {
+        console.log(error)
+        this.$confirm('无法加载 tileset，去处理？', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        })
+          .then(() => {
+            window.open('https://lab.earthsdk.com/model/', '_blank')
+          })
+          .catch(() => {})
+      })
     viewer.zoomTo(this.tileset) // 视角切换到模型的位置
 
     // const new_tileset = new Cesium.Cesium3DTileset({
